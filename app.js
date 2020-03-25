f1929d97b3be66ca1307235172d9a89e44cdfc5f
Aarthi <aarthi.hingorani@sitechhs.com>
2020-03-25 11:10:50 -0400

    3/25/20

---
+++ app.js
@@ -0,0 +1,62 @@
+/* let string = "I am a string";
+let sentence = "Hello Angel " + "and Vicky";
+let combine = "string + sentence"; */
+
+/* let student= "Sandra";
+let student2= "Gia";
+let courseName= "AP CSP";
+let teacherName= "Whalen";
+let sentence= student + " and " +student2 + " are taking " + courseName + " with " + teacherName;
+
+let betterSentence = `${student} and ${student2} are taking ${courseName} with ${teacherName} next year`;
+ */
+
+ //Numbers and Integers
+/*  let year= 2020;
+ let nextYear= year+1;
+ let past= year/2;
+ let future= year*2; */
+
+ //Booleans
+ //let trueStatement= false;
+ 
+ //Null and Undefined
+ //let richard;
+
+ //Boolean Logic or if/else
+ //let age= 18;
+ //let baoSquared= "Bao Bao";
+ //console.log(baoSquared === "bao Bao");
+ //console.log(age === "17");
+
+/* let age=45;
+if(age <18){
+    console.log("You are a child");
+} else if(age == 18){
+    console.log("Happy Birthday")
+} else{
+    console.log("You are over 18")
+} */
+
+//red,green,yellow light
+/* let color="red";
+
+if(color === red){
+    console.log("stop");
+} else if(color === green){
+    console.log("go");
+} else(color === yellow){
+    console.log("wait");
+} */
+
+//kristy and bao bao
+let kristyIntolerant= false;
+let baoBaoIntolerant= false;
+
+if (kristyIntolerant === true && baoBaoIntolerant === true){
+    console.log("almond milk");
+} else if (kristyIntolerant === true || baoBaoIntolerant === true){
+    console.log("both milks");
+} else {
+    console.log("regular milk");
+}
